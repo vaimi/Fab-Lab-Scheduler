7e914f8a8f338a2efa7993b273351673da01ee1f
Mikko Väisänen <mjvaisanen@gmail.com>
2015-10-08 14:21:03 +0300

    Combined sub-pages controllers under parent controller (info, reservations)

--- application/controllers/info.php
+++ application/controllers/info.php
@@ -1,12 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class FloorPlan extends CI_Controller
+class Info extends CI_Controller
 {
 	public function __constructor() {
 		parent::__constructor();
 	}
 	// this is the home page
 	public function index() {
-
+		$this->load->view('info/index');
+	}
+	
+	public function floorplan() {
 		$this->load->view('info/floorplan');
+	}
+	
+	public function machines() {
+		$this->load->view('info/machines');
 	}
 }
--- application/controllers/reservations.php
+++ application/controllers/reservations.php
@@ -1,12 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class active extends CI_Controller
+class Reservations extends CI_Controller
 {
 	public function __constructor() {
 		parent::__constructor();
 	}
 	// this is the home page
 	public function index() {
-
+		$this->load->view('reservations/index');
+	}
+	
+	public function active() {
+		$this->load->view('reservations/active');
+	}
+	
+	public function reserve() {
 		$this->load->view('reservations/active');
 	}
 }
