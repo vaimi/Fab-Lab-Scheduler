787e9bf7c409cddb37118c564993db0bb0803ea6
Mikko Väisänen <mjvaisanen@gmail.com>
2015-10-08 14:46:47 +0300

    Added some jumbotron stuff

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -9,7 +9,7 @@
  
 	<!-- Bootstrap -->
     <link href="<?php echo asset_url();?>css/bootstrap.min.css" rel="stylesheet" type="text/css"> 
-
+	<link href="<?php echo asset_url();?>css/bootstrap-fablab.css" rel="stylesheet" type="text/css"> 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- application/views/partials/jumbotron_center.php
+++ application/views/partials/jumbotron_center.php
@@ -1,4 +1,4 @@
-    <div class="jumbotron">
+	<div class="vertical-center text-center jumbotron">
       <div class="container">
         <h1><?php echo $title;?></h1>
         <p><?php echo $message;?></p>
