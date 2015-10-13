0c9bfdc2ed662ee1e1f8fa0fb30229b29870add3
Mikko Väisänen <mjvaisanen@gmail.com>
2015-10-13 14:23:03 +0300

    Added jumbotron background image, currently same on every page.

--- application/views/partials/jumbotron.php
+++ application/views/partials/jumbotron.php
@@ -1,4 +1,4 @@
-<div class="jumbotron container">
+<div class="jumbotron container jumbotron-info img-rounded">
 	<div class="container">
 		<h2><?php echo $title;?></h1>
 		<p><?php echo $message;?></p>
