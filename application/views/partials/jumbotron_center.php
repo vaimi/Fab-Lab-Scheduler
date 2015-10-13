b9923a8187fa169f770183c00c9efd1858c517fb
Mikko Väisänen <mjvaisanen@gmail.com>
2015-10-13 14:40:48 +0300

    home screen background. Again, needs still work.

--- application/views/partials/jumbotron_center.php
+++ application/views/partials/jumbotron_center.php
@@ -1,4 +1,4 @@
-	<div class="vertical-center text-center jumbotron">
+	<div class="vertical-center text-center jumbotron jumbotron-full-home">
       <div class="container">
         <h1><?php echo $title;?></h1>
         <p><?php echo $message;?></p>
