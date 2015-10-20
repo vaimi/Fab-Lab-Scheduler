1ec4924e2b4678f4f42ef7bc94b2c6bd6f0dfcf8
Mikko Väisänen <mjvaisanen@gmail.com>
2015-10-20 17:19:21 +0300

    Some timetable stuff

---
+++ application/views/admin/timetable.php
@@ -0,0 +1,18 @@
+<div class="container">
+	<div class="btn-toolbar">
+		<button type="button" class="btn btn-success">
+			<span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Save
+			<!-- Modal with two calendars -->
+		</button>
+		<span class="btn-separator"></span>
+		<button type="button" class="btn btn-primary">
+			<span class="glyphicon glyphicon-copy" aria-hidden="true"></span> Copy schedule...
+			<!-- Modal with two calendars -->
+		</button>
+		<button type="button" class="btn btn-danger">
+			<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Delete schedules...
+			<!-- Modal with selectable days -->
+		</button>
+	</div>
+	<p>see http://fullcalendar.io/js/fullcalendar-2.4.0/demos/external-dragging.html</p>
+</div>
