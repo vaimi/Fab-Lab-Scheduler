0f135401d4d2305db1fa857771128ac382396b51
Mikko Väisänen <mjvaisanen@gmail.com>
2015-10-20 22:52:26 +0300

    half of the users management page (static)

---
+++ application/views/admin/users.php
@@ -0,0 +1,31 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+			<h2>Search for user</h2>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="input-group input-group-lg">
+						<input type="text" class="form-control input-lg" id="search_people" placeholder="Search by email, name, phone...">
+							<span class="input-group-btn">
+								<button class="btn btn-default btn-lg" type="submit">Search</button>
+							</span>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<h4>Results</h4>
+					<div class="list-group">
+					    <a href="#" class="list-group-item active">Mikko Väisänen</a>
+						<a href="#" class="list-group-item">Markus Särkiniemi</a>
+						<a href="#" class="list-group-item">Thang Luu</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<h2>User data</h2>
+			Lorem ipsum
+		</div>
+	</div>
+</div>
