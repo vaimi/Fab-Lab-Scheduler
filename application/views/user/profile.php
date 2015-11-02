4c3ade73cb2dac893eafc00e2201909f163f4b03
Urkki <urkeloo@gmail.com>
2015-11-02 21:54:14 +0200

    Fixed get_conversiations. Messed up with this conflict.

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -35,6 +35,21 @@
 			}
 		});
 	}
+	function get_conversations()
+	{
+		$('#user_data_form').fadeOut('fast');
+		$.ajax({
+			type: "POST",
+			url: "<?php echo base_url('user/get_conversations'); ?>",
+			success: function(data) {
+				// return success
+				if (data.length > 0) {
+					$('#user_data_form').html(data);
+					$('#user_data_form').fadeIn('fast');
+				}
+			}
+		});
+	}
 </script>
 
 <div class="container">
@@ -44,7 +59,7 @@
 				<ul class="nav nav-pills nav-stacked" id="profile_menu">
 					<li class="active"><a href="#" onclick="get_user_profile();">Profile</a></li>
 					<li><a href="#" onclick="get_machine_levels();">Machine skill levels</a></li>
-					<li><a href="#">Messages</a></li>
+					<li><a href="#" onclick="get_conversations();">Messages</a></li>
 					<li><a href="#">Reservations</a></li>
 				</ul>
 			</div>
