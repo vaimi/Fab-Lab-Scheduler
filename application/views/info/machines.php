239873b08258c6a89dea06982304d024463c7bc1
Urkki <urkeloo@gmail.com>
2015-12-15 15:23:49 +0200

    Added rows to info page

--- application/views/info/machines.php
+++ application/views/info/machines.php
@@ -1,7 +1,11 @@
 <div class="container">
-<div class="row">
 <?php
+	$i = 0;
 foreach ($mdata as $machine) {
+	if ($i % 3 == 0)
+	{
+		echo '<div class="row">';
+	}
 	echo '<div class="col-sm-6 col-md-4">';
 	echo '	<div class="thumbnail">';
 	//echo '		<img src='.asset_url().'images/'.$machine['image'].' alt="Machine image" class="img-rounded">';
@@ -12,6 +16,11 @@ foreach ($mdata as $machine) {
 	echo '		</div>';
 	echo '	</div>';
 	echo '</div>';
+	$i++;
+	if ($i % 3 == 0)
+	{
+		echo '</div>';
+	}
 }
 ?>
 </div>
