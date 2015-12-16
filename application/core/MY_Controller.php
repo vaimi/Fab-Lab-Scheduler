a6c51ad1e4170bfe5ace4e9e3f1bf83f27ed7567
Mikko Vaisanen <mjvaisanen@gmail.com>
2015-12-16 08:34:05 +0200

    repopulate also first_name on session expire

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -8,12 +8,13 @@ class MY_Controller extends CI_Controller
 
 	public function check_and_set_session_variable()
 	{
-		if (!isset($this->session->surname))
+		if (!isset($this->session->surname) or !isset($this->session->first_name))
 		{
 			if($this->aauth->is_loggedin())
 			{
 				$this->load->model('User_model');
 				$session_variables = $this->User_model->get_session_data($this->session->id);
+				$this->session->set_userdata('first_name', $session_variables->first_name);
 				$this->session->set_userdata('surname', $session_variables->surname);
 			}
 		}
