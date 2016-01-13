bb1d4ba22029bff8606c7a4411fe8f06e7f38e4f
Mikko Väisänen <mjvaisanen@gmail.com>
2016-01-13 17:53:16 +0200

    Redone registration form. Added more client side validation
    Still needs to sorten out duplicate server side validation

--- application/views/modals/register.php
+++ application/views/modals/register.php
@@ -1,3 +1,5 @@
+<script src="<?php echo asset_url();?>js/validator.min.js"></script>
+
 <div id="registerModal" class="modal fade" role="dialog">
 	<div class="modal-dialog">
 		<!-- Modal content-->
@@ -6,75 +8,90 @@
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
 				<h4 class="modal-title">REGISTRATION</h4>
 			</div>
+			<form name="registration" id="registerform" method="post" action="<?php echo base_url();?>user/registration" onsubmit="return true;">
 			<div class="modal-body">
-				<form name="registration" method="post" action="<?php echo base_url();?>user/registration"
-					onsubmit="return true;">
-					<table>
-						<tr>
-							<td width="150px"><label for="username">User name *</label></td>
-							<td><input type="text" class="form-control" name="username" id="username"
-								style="width: 200px;" value="" required="" autofocus="" placeholder="User name" /></td>
-						</tr>
-						<tr>
-							<td><label for="password">Password *</label></td>
-							<td><input type="password" class="form-control" name="first_password" id="first_password"
-								style="width: 200px;" value="" required="" autofocus="" placeholder="Password" /></td>
-						</tr>
-						<tr>
-							<td><label for="password">Confirm Password *</label></td>
-							<td><input type="password" class="form-control" name="second_password" id="second_password"
-								style="width: 200px;" value="" required="" autofocus="" placeholder="Password" /></td>
-						</tr>
-						<tr>
-							<td><label for="first_name">First name *</label></td>
-							<td><input type="text" class="form-control" name="first_name" id="first_name"
-								style="width: 200px;" value="" required="" autofocus="" placeholder="First name" /></td>
-						</tr>
-						<tr>
-							<td><label for="surname">Last name *</label></td>
-							<td><input type="text" class="form-control" name="surname" id="surname"
-								style="width: 200px;" value="" required="" autofocus="" placeholder="Last name" /></td>
-						</tr>
-			
-						<tr>
-							<td><label for="email">Email address *</label></td>
-							<td><input type="email" class="form-control" name="email" id="email" style="width: 200px;"
-								value="" required="" autofocus="" placeholder="Email address" /></td>
-						</tr>
-						<tr>
-							<td><label for="phone_number">Phone number</label></td>
-							<td><input type="text" class="form-control" name="phone_number" id="phone_number" style="width: 200px;"
-								value="" required="" autofocus="" placeholder="Phone number" /></td>
-						</tr>
-						<tr>
-							<td><label for="company">Company</label></td>
-							<td><input type="text" class="form-control" name="company" id="company"
-								style="width: 200px;" value="" autofocus="" placeholder="Company" /></td>
-						</tr>
-						<tr>
-							<td><label for="address_street">Address</label></td>
-							<td><input type="text" class="form-control" name="address_street" id="address_street"
-								style="width: 200px;" value="" autofocus="" placeholder="Address" /></td>
-						</tr>
-						<tr>
-							<td><label for="address_postal_code">Postal code</label></td>
-							<td><input type="text" class="form-control" name="address_postal_code" id="address_postal_code"
-								style="width: 200px;" value="" autofocus="" placeholder="Postal code" /></td>
-						</tr>
-						<tr>
-							<td><label for="student_number">Student number</label></td>
-							<td><input type="text" class="form-control" name="student_number" id="student_number"
-								style="width: 200px;" value="" autofocus="" placeholder="Student number" /></td>
-						</tr>
-						<tr>
-							<td><button type="submit" class='btn btn-primary' text="Register" >Register</button></td>
-						</tr>
-					</table>
-				</form>
+				<div class="form-group required has-feedback">
+					<label class="control-label" for="username">User name</label>
+					<input type="text" data-minlength="5" data-maxlength="100" class="form-control" name="username" id="username" required placeholder="User name" />
+					<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+					<span class="help-block">Length between 5 and 100 characters.</span>
+				</div>	
+				<div class="form-group required has-feedback">
+					<label class="control-label" for="email">Email address</label>
+					<input type="email" class="form-control" name="email" id="email" required placeholder="Email address" />
+					<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+				</div>
+				<div class="form-group required has-feedback">
+					<label class="control-label">Password</label>
+					<div class="row">
+						<div class="col-sm-6">
+							<input type="password" data-minlength="5" data-maxlength="100" class="form-control" name="first_password" id="first_password" value="" required placeholder="Password" />
+							<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+							<span class="help-block">Length between 5 and 20 characters.</span>
+						</div>
+						<div class="col-sm-6">
+							<input type="password" class="form-control" name="second_password" id="second_password" data-match="#first_password" data-match-error="Whoops, these aren't the same" value="" required placeholder="Confirm password" />
+							<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+							<div class="help-block with-errors"></div>
+						</div>
+					</div>
+				</div>
+				<div class="form-group required has-feedback">
+					<label class="control-label">Name</label>
+					<div class="row">
+						<div class="col-sm-6">
+							<input type="text" class="form-control" name="first_name" id="first_name" placeholder="First name" required/>
+							<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+						</div>
+						<div class="col-sm-6">
+							<input type="text" class="form-control" name="surname" id="surname" placeholder="Last name" required/>
+							<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+						</div>
+					</div>
+				</div>
+				<div class="form-group required has-feedback">
+					<label class="control-label" for="phone_number">Phone number</label>
+					<input type="tel" class="form-control" name="phone_number" id="phone_number" required placeholder="Phone number" />
+					<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+				</div>
+				<div class="form-group has-feedback">
+					<label class="control-label" for="address_street">Address</label>
+					<div class="row">
+						<div class="col-sm-6">
+							<input type="text" class="form-control" name="address_street" id="address_street" placeholder="Address" />
+							<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+						</div>
+						<div class="col-sm-6">
+							<input type="text" class="form-control" name="address_postal_code" id="address_postal_code" placeholder="Postal code" />
+							<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+						</div>
+					</div>
+				</div>
+				<div class="form-group has-feedback">
+					<label class="control-label" for="company">Company</label>
+					<input type="text" class="form-control" name="company" id="company" placeholder="Company" />
+					<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+				</div>
+				<div class="form-group has-feedback">
+					<label class="control-label" for="student_number">Student number</label>
+					<input type="text" class="form-control" name="student_number" id="student_number" placeholder="Student number" />
+					<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+				</div>
 			</div>
 			<div class="modal-footer">
+				<button type="submit" class='btn btn-primary' text="Register" >Register</button>
 				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 			</div>
+			</form>
 		</div>
 	</div>
 </div>
+<script>
+$('#registerform').validator().on('submit', function (e) {
+    if (e.isDefaultPrevented()) {
+    	alert('form is not valid');
+    } else {
+        
+    }
+});
+</script>
--- application/views/registration_form.php
+++ application/views/registration_form.php
@@ -2,74 +2,69 @@
  
 <?php $this->load->view('partials/menu'); ?>
 
+<script src="<?php echo asset_url();?>js/validator.min.js"></script>
+
 <div class="container">
 	<h4 class="modal-title">REGISTRATION</h4>
 	<hr/>
 	<?php foreach ($errors as $item):?>
 		<div style='color:red;'>- <?php echo $item;?></div>
 	<?php endforeach;?>
-	<form name="registration" method="post" action="<?php echo base_url();?>user/registration" onsubmit="return true;">
-		<table>
-			<tr>
-				<td width="150px"><label for="username">User name *</label></td>
-				<td><input type="text" class="form-control" name="username" id="username"
-					style="width: 200px;" value="<?php echo $username;?>" required="" autofocus="" placeholder="User name" /></td>
-			</tr>
-			<tr>
-				<td><label for="password">Password *</label></td>
-				<td><input type="password" class="form-control" name="first_password" id="first_password"
-					style="width: 200px;" value="" required="" autofocus="" placeholder="Password" /></td>
-			</tr>
-			<tr>
-				<td><label for="password">Retype Password *</label></td>
-				<td><input type="password" class="form-control" name="second_password" id="second_password"
-					style="width: 200px;" value="" required="" autofocus="" placeholder="Password" /></td>
-			</tr>
-			<tr>
-				<td><label for="first_name">First name *</label></td>
-				<td><input type="text" class="form-control" name="first_name" id="first_name"
-					style="width: 200px;" value="<?php echo $first_name;?>" required="" autofocus="" placeholder="First name" /></td>
-			</tr>
-			<tr>
-				<td><label for="surname">Last name *</label></td>
-				<td><input type="text" class="form-control" name="surname" id="surname"
-					style="width: 200px;" value="<?php echo $surname;?>" required="" autofocus="" placeholder="Last name" /></td>
-			</tr>
-	
-			<tr>
-				<td><label for="email">Email address *</label></td>
-				<td><input type="email" class="form-control" name="email" id="email" style="width: 200px;"
-					value="<?php echo $email;?>" required="" autofocus="" placeholder="Email address" /></td>
-			</tr>
-			<tr>
-				<td><label for="phone_number">Phone number</label></td>
-				<td><input type="text" class="form-control" name="phone_number" id="phone_number" style="width: 200px;"
-					value="<?php echo $phone_number;?>" required="" autofocus="" placeholder="Phone number" /></td>
-			</tr>
-			<tr>
-				<td><label for="company">Company</label></td>
-				<td><input type="text" class="form-control" name="company" id="company"
-					style="width: 200px;" value="<?php echo $company;?>" autofocus="" placeholder="Company" /></td>
-			</tr>
-			<tr>
-				<td><label for="address_street">Address</label></td>
-				<td><input type="text" class="form-control" name="address_street" id="address_street"
-					style="width: 200px;" value="<?php echo $address_street;?>" autofocus="" placeholder="Address" /></td>
-			</tr>
-			<tr>
-				<td><label for="address_postal_code">Postal code</label></td>
-				<td><input type="text" class="form-control" name="address_postal_code" id="address_postal_code"
-					style="width: 200px;" value="<?php echo $address_postal_code;?>" autofocus="" placeholder="Postal code" /></td>
-			</tr>
-			<tr>
-				<td><label for="student_number">Student number</label></td>
-				<td><input type="text" class="form-control" name="student_number" id="student_number"
-					style="width: 200px;" value="<?php echo $student_number;?>" autofocus="" placeholder="Student number" /></td>
-			</tr>
-			<tr>
-				<td><button type="submit" class='btn btn-primary' text="Register" >Register</button></td>
-			</tr>
-		</table>
+	<form data-toggle="validator" name="registration" method="post" action="<?php echo base_url();?>user/registration" onsubmit="return true;">
+
+		<div class="form-group">
+			<label for="username">User name</label>
+			<input type="text" class="form-control" name="username" id="username" value="<?php echo $username;?>" required placeholder="User name" />
+		</div>	
+
+		<div class="form-group">
+			<label for="password">Password</label>
+			<div class="form-group col-sm-6">
+				<input type="password" class="form-control" name="first_password" id="first_password" value="" required placeholder="Password" />
+				<span class="help-block">Minimum of 6 characters</span>
+			</div>
+			<div class="form-group col-sm-6">
+				<input type="password" class="form-control" name="second_password" id="second_password" value="" required placeholder="Confirm password" />
+				<div class="help-block with-errors"></div>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="first_name">Name</label>
+			<div class="form-group col-sm-6">
+				<input type="text" class="form-control" name="first_name" id="first_name" value="<?php echo $first_name;?>" required placeholder="First name" />
+			</div>
+			<div class="form-group col-sm-6">
+				<input type="text" class="form-control" name="surname" id="surname" value="<?php echo $surname;?>" required placeholder="Last name" />
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="email">Email address</label>
+			<input type="email" class="form-control" name="email" id="email" value="<?php echo $email;?>" required placeholder="Email address" />
+		</div>
+		<div class="form-group">
+			<label for="phone_number">Phone number</label>
+			<input type="text" class="form-control" name="phone_number" id="phone_number" value="<?php echo $phone_number;?>" required placeholder="Phone number" />
+		</div>
+
+		<div class="form-group">
+			<label for="company">Company</label>
+			<input type="text" class="form-control" name="company" id="company" value="<?php echo $company;?>" placeholder="Company" />
+		</div>
+
+		<div class="form-group">
+			<label for="address_street">Address</label>
+			<div class="form-group col-sm-6">
+				<input type="text" class="form-control" name="address_street" id="address_street" value="<?php echo $address_street;?>" placeholder="Address" />
+			</div>
+			<div class="form-group col-sm-6">
+				<input type="text" class="form-control" name="address_postal_code" id="address_postal_code" value="<?php echo $address_postal_code;?>" placeholder="Postal code" />
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="student_number">Student number</label>
+			<input type="text" class="form-control" name="student_number" id="student_number" value="<?php echo $student_number;?>" placeholder="Student number" />
+		</div>
+		<button type="submit" class='btn btn-primary' text="Register" >Register</button>
 	</form>
 
 </div>
