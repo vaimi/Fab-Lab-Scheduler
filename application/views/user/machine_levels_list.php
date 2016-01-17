4da405a8d324f037b7b905279a9c0dd6dcb3f27a
Mikko Väisänen <mjvaisanen@gmail.com>
2016-01-17 20:24:58 +0200

    Cleaned up levels list on profile

--- application/views/user/machine_levels_list.php
+++ application/views/user/machine_levels_list.php
@@ -1,13 +1,13 @@
+<script type="text/javascript" src="<?=asset_url()?>js/bootstrap-rating.min.js"></script>
 <script src="<?php echo asset_url();?>js/sortable.min.js"></script>
 <table class="sortable-theme-bootstrap table table-striped" data-sortable>
 	<thead>
     	<tr>
             <th data-sorted="true" data-sorted-direction="ascending">MID</th>
             <th>Machine name</th>
-            <th>Skill level</th>
             <th>Manufacturer</th>
             <th>Model</th>
-            <th>Description</th>
+            <th>Skill level</th>
        </tr>
    </thead>
    <tbody>
@@ -15,10 +15,9 @@
 	  <tr>
 	  	<td><?php echo $r['mid']?></td>
 	  	<td><?php echo $r['machine_name']?></td>
-	  	<td><?php echo $r['level']?></td>
 	    <td><?php echo $r['manufacturer']?></td>
 	    <td><?php echo $r['model']?></td>
-	    <td><?php echo $r['description']?></td>
+      <td data-value="<?php echo $r['level']?>"><input type="hidden" class="rating" data-readonly value="<?php echo $r['level']?>"></td>
 	  </tr>
 	  <?php }?>
    </tbody>
