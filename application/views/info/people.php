545475e502f67af3def2beccc1103d417c60a8e8
Mikko Vaisanen <mjvaisanen@gmail.com>
2016-01-20 23:41:55 +0200

    removed all emails

--- application/views/info/people.php
+++ application/views/info/people.php
@@ -7,15 +7,15 @@
 	<hr />
 	<div style="float:left; width:200px;text-align:center;font-weight:bold">
 		<div><img src="<?php echo base_url('assets/data/site_people/profile_pic_mikkor.jpg'); ?>" style="max-width:300px;max-height:300px;" /></div>
-		<div>Mikko Vaisanen (***REMOVED***)</div>
+		<div>Mikko Vaisanen ()</div>
 	</div>
 	<div style="float:left; width:200px;text-align:center;font-weight:bold;margin-left:200px;">
 		<div><img src="<?php echo base_url('assets/data/site_people/profile_pic_thang.jpg'); ?>" style="max-width:300px;max-height:300px;" /></div>
-		<div>Thang Luu (***REMOVED***)</div>
+		<div>Thang Luu ()</div>
 	</div>
 	<div style="float:left; width:200px;text-align:center;font-weight:bold;margin-left:200px;">
 		<div><img src="<?php echo base_url('assets/data/site_people/profile_pic_markus.jpg'); ?>" style="max-width:300px;max-height:300px;" /></div>
-		<div>Markus Sarkiniemi (***REMOVED***)</div>
+		<div>Markus Sarkiniemi ()</div>
 	</div>
 	<div style="clear:both;"></div>
 	
